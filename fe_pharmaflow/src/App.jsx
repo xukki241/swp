33f83c574495b598737f1cca60040bf17a8584f1
huytran303 <tnh.t1k30.ht@gmail.com>
2025-09-23 13:47:05 +0700

    add login page

--- fe_pharmaflow/src/App.jsx
+++ fe_pharmaflow/src/App.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from "@/layout/Layout"
+import Login from "@/pages/LoginPage"
 
 export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </Router>
+  )
+}
+
+function Dashboard() {
   const breadcrumbItems = [
     { title: "Building Your Application", href: "#" },
     { title: "Data Fetching" }
