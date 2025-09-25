4bf011bdaad4f9658a5dc5bf85f89933f03b7707
huytran303 <tnh.t1k30.ht@gmail.com>
2025-09-25 14:39:54 +0700

    fix lai eslint config

--- fe_pharmaflow/src/layout/AuthLayout.jsx
+++ fe_pharmaflow/src/layout/AuthLayout.jsx
@@ -1,7 +1,7 @@
 import { Pill } from "lucide-react"
 import { Link } from "react-router-dom"
 import { Outlet } from "react-router-dom"
-export default function AuthLayout({ children, title, subtitle }) {
+export default function AuthLayout() {
     return (
         <div className="grid min-h-svh lg:grid-cols-2">
             <div className="flex flex-col gap-4 p-6 md:p-10">
