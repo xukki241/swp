89ccefca7724a74d966c173abdd894813a4adf86
huytran303 <tnh.t1k30.ht@gmail.com>
2025-09-25 16:59:16 +0700

    fix lai eslint config

--- fe_pharmaflow/src/pages/LoginPage.jsx
+++ fe_pharmaflow/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/login-form"
-import AuthLayout from "@/layout/AuthLayout"
+
 
 export default function LoginPage() {
   return (
--- fe_pharmaflow/src/pages/RegisterPage.jsx
+++ fe_pharmaflow/src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import { RegisterForm } from "@/components/register-form"
-import AuthLayout from "@/layout/AuthLayout"
+
 
 export default function RegisterPage() {
   return (
