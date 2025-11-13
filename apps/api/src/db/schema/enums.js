b1dd190686ed7cfe542dfc42cde23e3d292d2e64
Quang Huy <ngquanghuy.work@gmail.com>
2025-11-13 09:52:38 +0700

    format

--- apps/api/src/db/schema/enums.js
+++ apps/api/src/db/schema/enums.js
@@ -54,9 +54,7 @@ export const purchaseOrderStatus = pgEnum("purchase_order_status", [
   "cancelled",
 ]);
 
-export const salesOrderStatus = pgEnum("sales_order_status", [
-  "paid",
-]);
+export const salesOrderStatus = pgEnum("sales_order_status", ["paid"]);
 
 export const salesOrderPaymentMethod = pgEnum("sales_order_payment_method", [
   "cash",
